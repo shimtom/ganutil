31da98fe7dfd852440b44f7868d8ab47017284b7
Tomoshige Shimomura <ii00zero1230@gmail.com>
2017-06-13 20:19:29 +0900

    様々な拡張子のファイルを読み込めるように

--- gan/discrimination.py
+++ gan/discrimination.py
@@ -1,24 +1,20 @@
-from numpy import array, savez
-from os.path import join, isfile
+from numpy import save, savez, savetxt
 
 
-def discriminate(discriminator, dataset, save, batch_size=32):
-    """discriminatorを使用してデータセットに対して識別を行う.
-    識別結果はsaverに指定されているディレクトリに`discriminated.npz`で保存される.
-    ファイルがすでに存在する場合は索引を付与して保存する.
+def discriminate(discriminator, dataset, save_path, batch_size=32):
+    """discriminatorを使用してデータセットに対して識別を行い、結果を保存する.
+    ファイルがすでに存在する場合は上書き保存を行う.
 
     :param keras.Model discriminator: discriminator モデル.
     :param numpy.ndarray dataset: データセット. 形状は[data size, discriminatorの入力形状].
-    :param str save: 結果を保存するディレクトリ.
+    :param str save_path: 結果を保存するファイル名.
     :param int batch_size: バッチサイズ.
     """
     result = discriminator.predict(dataset, batch_size=batch_size)
-
-    path = join(save, 'discriminated.npz')
-    if isfile(path):
-        index = 1
-        while not isfile(join(save, 'discriminated(%d).npz' % index)):
-            index += 1
-        path = join(save, 'discriminated(%d).npz' % index)
-
-    savez(path, array(result))
+    extension = save_path.split('.')[-1]
+    if extension is 'npy' or save_path.find('.') == -1:
+        save(save_path, result)
+    elif extension is 'npz':
+        savez(save_path, result)
+    else:
+        savetxt(save_path, result)
--- gan/generation.py
+++ gan/generation.py
@@ -1,23 +1,20 @@
-from numpy import array, savez
-from os.path import join, isfile
+from numpy import save, savez, savetxt
 
 
-def generate(generator, dataset, save, batch_size=32):
-    """generatorを使用してデータセットから生成を行う.
-    識別結果はsaveに指定されているディレクトリに`generated.npz`で保存される.
-    ファイルがすでに存在する場合は索引を付与して保存する.
+def generate(generator, dataset, save_path, batch_size=32):
+    """generatorを使用してデータセットから生成を行い、結果を保存する
+    ファイルがすでに存在する場合は上書き保存を行う.
 
     :param keras.Model generator: generator モデル.
     :param numpy.ndarray dataset: データセット. 形状は[data size, generatorの入力形状].
-    :param str save: 結果を保存するディレクトリ.
+    :param str save_path: 結果を保存するファイル名.
     :param int batch_size: バッチサイズ.
     """
     result = generator.predict(dataset, batch_size=batch_size)
-    path = join(save, 'generated.npz')
-    if isfile(path):
-        index = 1
-        while not isfile(join(save, 'generated(%d).npz' % index)):
-            index += 1
-        path = join(save, 'generated(%d).npz' % index)
-
-    savez(path, array(result))
+    extension = save_path.split('.')[-1]
+    if extension is 'npy' or save_path.find('.') == -1:
+        save(save_path, result)
+    elif extension is 'npz':
+        savez(save_path, result)
+    else:
+        savetxt(save_path, result)
