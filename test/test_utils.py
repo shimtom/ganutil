0b15552fc1fa00b047201ff4f3d41001996a2485
Tomoshige Shimomura <ii00zero1230@gmail.com>
2017-07-31 16:17:07 +0900

    不要なコードを削除.pytestで修正

--- test/test_utils.py
+++ test/test_utils.py
@@ -1,76 +1,42 @@
 import json
 import os
 import tempfile
-from unittest import TestCase
 
 import ganutil.utils as utils
+import pytest
 import yaml
 from keras.layers import Dense
-from keras.losses import binary_crossentropy
 from keras.models import Sequential
-from keras.optimizers import SGD
 
 
-class TestUtils(TestCase):
-    def test_compile(self):
-        discriminator = Sequential()
-        discriminator.add(Dense(1, input_shape=(16,)))
-        dparameter = {
-            'optimizer': SGD(),
-            'loss': binary_crossentropy
-        }
-        generator = Sequential()
-        generator.add(Dense(16, input_shape=(32,)))
-        gparameter = {
-            'optimizer': SGD(),
-            'loss': binary_crossentropy
-        }
-        gan, discriminator, generator = utils.compile(
-            discriminator, generator, dparameter, gparameter)
+@pytest.fixture()
+def dirpath():
+    tempdir = tempfile.TemporaryDirectory()
+    print('Directory: %s' % tempdir.name)
+    yield tempdir.name
+    print('Directory clean upped.')
+    tempdir.cleanup()
 
-        self.assertTrue(gan.built)
-        self.assertTrue(discriminator.built)
-        self.assertFalse(generator.built)
 
-    def test_save_architecture(self):
-        with tempfile.TemporaryDirectory() as dirpath:
-            discriminator = Sequential()
-            discriminator.add(Dense(1, input_shape=(16,)))
-            generator = Sequential()
-            generator.add(Dense(16, input_shape=(32,)))
+@pytest.mark.util
+@pytest.mark.parametrize('de, ge, dload, gload', [
+    ('.yml', '.yml', yaml.load, yaml.load),
+    ('.json', '.yml', json.load, yaml.load),
+    ('.yml', '.json', yaml.load, json.load),
+    ('.json', '.json', json.load, json.load),
+])
+def test_save_architecture(dirpath, de, ge, dload, gload):
+    discriminator = Sequential()
+    discriminator.add(Dense(1, input_shape=(16,)))
+    generator = Sequential()
+    generator.add(Dense(16, input_shape=(32,)))
 
-            dpath = os.path.join(dirpath, 'discriminator')
-            gpath = os.path.join(dirpath, 'generator')
+    dpath = os.path.join(dirpath, 'discriminator')
+    gpath = os.path.join(dirpath, 'generator')
 
-            for de in ['.yml', '.json', '']:
-                for ge in ['.yml', '.json', '']:
-                    if de == '' or ge == '':
-                        with self.assertRaises(ValueError):
-                            utils.save_architecture(
-                                dpath + de, gpath + ge, discriminator, generator)
-                    else:
-                        utils.save_architecture(
-                            dpath + de, gpath + ge, discriminator, generator)
-                        with open(dpath + de) as d:
-                            try:
-                                if de == '.yml':
-                                    yaml.load(d)
-                                elif de == '.json':
-                                    json.load(d)
-                                else:
-                                    raise(ValueError('Unknown `de`: ' + str(de)))
-                            except Exception as e:
-                                self.fail(e)
-                        with open(gpath + ge) as g:
-                            try:
-                                if ge == '.yml':
-                                    yaml.load(g)
-                                elif ge == '.json':
-                                    json.load(g)
-                                else:
-                                    raise(ValueError('Unknown `ge`: ' + str(ge)))
-                            except Exception as e:
-                                self.fail(e)
+    utils.save_architecture(dpath + de, gpath + ge, discriminator, generator)
+    with open(dpath + de) as d:
+        dload(d)
 
-    def test_set_trainability(self):
-        pass
+    with open(gpath + ge) as g:
+        gload(g)
